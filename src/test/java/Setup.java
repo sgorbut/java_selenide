8cc1d960f5e8d009797dd73e85ac585b69038854
sgorbut <sgorbut@gmail.com>
2022-11-16 15:14:37 +0300

    added setup

---
+++ src/test/java/Setup.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Setup {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        BasePage.setDriver(driver);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+
+}
